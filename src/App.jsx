6e208a9d8fd033b46e9f7156166969b8ca452db6
Abdallah Ahmed <abldalla.ahmed@ilerra.com>
2024-05-09 14:31:06 +0300

    t-care (upload files)

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MainNav from "./components/MainNav";
+
+
+export default function App() {
+  return (
+    <div>
+      <MainNav/>
+    </div>
+  )
+}
